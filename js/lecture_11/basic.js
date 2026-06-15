fef65f03c44acfcdd71d3398cbbd3c1f0a441111
kanishka-80 <kanishkagupta0802@gmail.com>
2026-06-15 11:41:14 +0530

    done

---
+++ js/lecture_11/basic.js
@@ -0,0 +1,28 @@
+  function createbankaccount(ib) {
+    let b= ib;
+    function deposit(a){
+        b+=a;
+        console.log(b);
+    }
+    function withdraw(a){
+        b+=a;
+        console.log(b);
+    }
+    function checkbalance(){
+        console.log(b);
+    }
+    return {deposit,withdraw,checkbalance};
+  }
+  const acc=createbankaccount(2000);
+  acc.deposit(500);
+  acc.withdraw(300);
+  acc.checkbalance();
+  console.log(acc.b);
+
+  function multiplier(factor){
+    return function(number){
+        return factor*number;
+    }
+  }
+  const t=multiplier(3);
+  console.log(t(20))
