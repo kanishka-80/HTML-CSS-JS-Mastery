318cbd8fdca67c6a6ed6281c58f6cd85ba8374e7
kanishka-80 <kanishkagupta0802@gmail.com>
2026-06-16 12:50:04 +0530

    done

---
+++ js/string.js
@@ -0,0 +1,40 @@
+//  length
+
+let name="   Kanishka  "
+console.log(name.length);
+
+// uppercase
+console.log(name.toUpperCase());
+
+// lowercase
+console.log(name.toLowerCase());
+
+// trim
+console.log(name.trim());
+
+// includes: check the specific string is present or not in main string
+let n="Vishal is not handsome"
+console.log(name.includes("not"));
+
+// startswith: checks whether the string strtswith that word
+console.log(n.startsWith("Vishal"));
+
+// endswith
+console.log(n.endsWith("so"));
+
+// indexOf: checks the is present on which index
+console.log(n.indexOf("is"));
+
+// slice: returns the elements within range
+console.log(n.slice(1,2))
+
+// substring
+console.log(n.substring(1,4))
+
+// relace
+console.log(n.replace("vishal","bunty"))
+
+// replaceall:replace all occurence
+console.log(n.replaceAll("vishal","bunty"))
+
+// split
