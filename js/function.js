52bb2fe0751895bd41ce434599d8ba622d204321
kanishka-80 <kanishkagupta0802@gmail.com>
2026-06-22 12:09:55 +0530

    done

---
+++ js/function.js
@@ -0,0 +1,28 @@
+function greet(name, callback){
+    console.log(`hello ${name}`);
+    callback();
+}
+
+function goodbye(){
+    console.log("goodbye");
+}
+greet("kanishka", goodbye);
+console.log("Download Started");
+ setTimeout(() =>{
+    console.log("Download Completed");
+ },5000);
+
+//  setInterval(() => {
+//     console.log("Show Data");
+//  },2000);
+
+//  To stop setInterval
+
+let count=0;
+const intervalId= setInterval(() => {
+    console.log("Show Data");
+    count++;
+    if(count >= 5){
+        clearInterval(intervalId);
+    }
+},1000);
